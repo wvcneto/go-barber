27683b5c7624519be67e57c3e5f37b1e16d13bb5
wvcneto <wvcneto@gmail.com>
2020-08-05 20:53:21 -0300

    jest and create-appointment-unit-test

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,21 @@
+import 'reflect-metadata';
+
+import CreateAppointmentService from './CreateAppointmentService';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment.', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123456789',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123456789');
+  });
+});
