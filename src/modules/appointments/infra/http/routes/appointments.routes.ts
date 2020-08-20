1c230563c4347590bfd60ef498edbf5ad258537e
wvcneto <wvcneto@gmail.com>
2020-08-20 17:48:04 -0300

    provider-appointments-schedule

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -3,22 +3,16 @@ import { Router } from 'express';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
+import ProviderAppointmentsController from '@modules/appointments/infra/http/controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-/*
-appointmentsRouter.get('/', async (request, response) => {
-  const appointments = await appointmentsRepository.find();
-
-  return response.json(appointments);
-});
-*/
-
-// http://localhost:3333/appointments
-
 appointmentsRouter.post('/', appointmentsController.create);
+
+appointmentsRouter.post('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
