1f76cab515eb4e089a97d64fc15b3e475e92f597
wvcneto <wvcneto@gmail.com>
2020-08-21 11:50:36 -0300

    middleware-routes-validations

--- src/modules/users/infra/http/routes/password.routes.ts
+++ src/modules/users/infra/http/routes/password.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ResetPasswordController from '@modules/users/infra/http/controllers/ResetPasswordController';
 import ForgotPasswordController from '@modules/users/infra/http/controllers/ForgotPasswordController';
@@ -7,8 +8,26 @@ const passwordRouter = Router();
 const resetPasswordController = new ResetPasswordController();
 const forgotPasswordController = new ForgotPasswordController();
 
-passwordRouter.post('/reset', resetPasswordController.create);
+passwordRouter.post(
+  '/forgot',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+    },
+  }),
+  forgotPasswordController.create,
+);
 
-passwordRouter.post('/forgot', forgotPasswordController.create);
+passwordRouter.post(
+  '/reset',
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().uuid().required(),
+      password: Joi.string().required(),
+      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+    },
+  }),
+  resetPasswordController.create,
+);
 
 export default passwordRouter;
